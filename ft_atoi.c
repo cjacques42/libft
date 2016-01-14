d9f51ce9e0394a70423d7d2a223f1b8e4bbe74b7
Corentin JACQUES <cjacques@e2r2p3.42.fr>
2016-01-14 18:13:13 +0100

    change atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cjacques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 10:12:25 by cjacques          #+#    #+#             */
-/*   Updated: 2015/11/26 10:37:58 by cjacques         ###   ########.fr       */
+/*   Updated: 2016/01/14 18:12:51 by cjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,18 @@ int		ft_atoi(const char *str)
 
 	i = 0;
 	result = 0;
-	neg = 0;
+	neg = 1;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
 			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-')
-		neg = 1;
+		neg = -1;
 	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result = result * 10 + (str[i] - 48);
+		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	if (neg == 1)
-		return (-result);
-	return (result);
+	return (result * neg);
 }
